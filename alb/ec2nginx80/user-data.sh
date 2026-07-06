7f5686ca73c944e5f2e28e572ecdaab229de5a71
csjin21c <csjin21c@gmail.com>
2026-07-07 08:29:36 +0900

    디렉토리 설정 변경

--- alb/ec2nginx80/user-data.sh
+++ alb/ec2nginx80/user-data.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 apt update -y
 apt install -y nginx
-curl -o /var/www/html/index.html 
-# sed -i "s/listen 80/listen 8080/g" /etc/nginx/sites-available/default
-# sed -i "s/listen \[::\]80/listen \[::\]8080/g" /etc/nginx/sites-available/default
+curl -o /var/www/html/index.html https://raw.githubusercontent.com/csjin21c/ex-aws/refs/heads/main/alb/ec2nginx80/index.html
 systemctl enable nginx
 systemctl restart nginx
